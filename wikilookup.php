8973822df5cbfe84194cc6cf00e0ce54465f8ac2
Moriel Schottlender <moriel@gmail.com>
2019-07-07 14:14:18 -0700

    Update plugin description

--- wikilookup.php
+++ wikilookup.php
@@ -1,11 +1,23 @@
 <?php
-/*
-* Plugin Name: Wikilookup
-* Description: Add popups from wikis to your content posts.
-* Version: 1.0
-* Author: Moriel Schottlender
-* Author URI: https://moriel.smarterthanthat.com
-*/
+/**
+ * Wikilookup for Wordpress
+ *
+ * @package     Wikilookup
+ * @author      Moriel Schottlender
+ * @copyright   2019 Moriel Schottlender
+ * @license     GPL-3.0
+ *
+ * @wordpress-plugin
+ * Plugin Name: Wikilookup
+ * Plugin URI:  http://moriel.smarterthanthat.com/
+ * Description: Description of the plugin.
+ * Version:     1.0.0
+ * Author:      Your Name
+ * Author URI:  http://moriel.smarterthanthat.com/
+ * Text Domain: wikilookup
+ * License:     GPL-3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ */
 include 'vendor/autoload.php';
 
 define( 'WIKILOOKUP_DIR_URL', plugin_dir_url( __FILE__ ) );
@@ -32,7 +44,6 @@ function wikilookup_shortcode( $atts , $content = null ) {
 	);
 }
 
-// add_filter( 'plugin_action_links', [ 'Wikilookup\Loader', 'addSettingsLink' ], 10, 5 );
 add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), [ 'Wikilookup\Loader', 'addSettingsLink' ], 10, 5 );
 
 // Register shortcode
