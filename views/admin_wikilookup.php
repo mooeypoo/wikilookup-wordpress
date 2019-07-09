9852fbd37d80b0f983c3dd2964de9bdadee16749
Moriel Schottlender <moriel@gmail.com>
2019-07-08 20:52:32 -0700

    Update latest wikilookup version; add credits to settings screen

--- views/admin_wikilookup.php
+++ views/admin_wikilookup.php
@@ -143,7 +143,9 @@ $wikilookup_nonce = wp_create_nonce( 'wikilookup_settings_form_nonce' );
 			</table>
 		</div>
 
-
 		<?php submit_button( 'Save settings', 'primary' ); ?>
 	</form>
+
+	<hr />
+	<p><a href="https://github.com/mooeypoo/wikilookup-wordpress" target="_blank">Wikilookup for Wordpress</a> was developed by <a href="http://moriel.smarterthanthat.com/" target="_blank">Moriel Schottlender</a>, under an open source license. Please consider <a href="https://github.com/mooeypoo/wikilookup-wordpress" target="_blank">contributing</a>.</p>
 </div>
